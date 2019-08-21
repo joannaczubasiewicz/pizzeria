16d943a69ca2a6edc130d25878185ef03cda7c3e
Joanna Czubasiewicz <joanna.czubasiewicz@gmail.com>
2019-08-21 07:17:52 +0200

    Add class CartProduct do script.js

--- src/js/script.js
+++ src/js/script.js
@@ -194,14 +194,55 @@
 
             /*create DOM element using utils.createElementFromHTML*/
 
-            const generatedDom = utils.createDOMFromHTML(generatedHTML);
+            const generatedDOM = utils.createDOMFromHTML(generatedHTML);
 
 
             /*add element to cart*/
-            thisCart.dom.productList.appendChild(generatedDom);
+            thisCart.dom.productList.appendChild(generatedDOM);
 
 
             console.log('adding product:', menuProduct);
+
+
+            thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+            console.log('thisCart.products:', thisCart.products);
+
+
+
+        }
+    }
+
+    class CartProduct {
+        constructor(menuProduct, element) {
+            const thisCartProduct = this;
+
+            thisCartProduct.id = menuProduct.id;
+            thisCartProduct.name = menuProduct.name;
+            thisCartProduct.price = menuProduct.price;
+            thisCartProduct.singlePice = menuProduct.singlePice;
+            thisCartProduct.amount = menuProduct.amount;
+
+            thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+            thisCartProduct.getElements(element);
+
+            console.log(thisCartProduct);
+            console.log('product data:', menuProduct);
+
+
+        }
+
+        getElements(element) {
+            const thisCartProduct = this;
+
+            thisCartProduct.dom = {};
+
+            thisCartProduct.dom.wrapper = element;
+            thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+            thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+            thisCartProduct.dom.dit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+            thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+
         }
     }
 
@@ -449,7 +490,7 @@
                     /* END LOOP: for each optionId in param.options */
                 }
                 /* END LOOP: for each paramId in thisProduct.data.params */
-                console.log(thisProduct.params);
+                //console.log(thisProduct.params);
             }
 
 
